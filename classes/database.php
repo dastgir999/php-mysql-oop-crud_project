a6a7788188458d21ba220e25b13f31af169eadb3
dastgir999 <alihanain278@gmail.com>
2024-12-27 16:51:28 +0000

    Update database.php

--- classes/database.php
+++ classes/database.php
@@ -13,7 +13,7 @@ class Database {
 		try{
 			$this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}",$this->username,$this->password);
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-		}catch(PDOExeption $e){
+		}catch(PDOException $e){
 			echo "connection Error:".$e->getMessage();
 		}
 
